92206932b976fd5eecf2971497b5d499ecadf360
GaureshGovenkar <94797817+GaureshGovenkar@users.noreply.github.com>
2024-06-20 20:28:05 +0530

    subnet-set-5

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "Node-cloud"
   node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids      = data.aws_subnets.public["us-east-1a"].id
+  subnet_ids      = data.aws_subnets.public.ids[0,1,2,3]
 
   scaling_config {
     desired_size = 1
